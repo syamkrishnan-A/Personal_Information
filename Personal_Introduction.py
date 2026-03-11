3a3c1177548c49d35de38ea7d40fe1da03f28189
Syankrishnan A <syamkrishnan680@gmail.com>
2026-03-11 15:49:07 +0530

    Personal_Introduction.py

--- Personal_Introduction.py
+++ Personal_Introduction.py
@@ -6,8 +6,9 @@ hobby=input("What is your favorite hobby?")
 
 
 print("\n--- Welcome Message ---")
-print(f"Hello {name}! 👋")
+print(f"Hello {name}!")
 print(f"You are {age} years old and you come from {place}.")
 print(f"You are studying at {college}.")
 print(f"It's great that you enjoy {hobby}.")
+
 print(f"Nice to meet you, {name}! Have a wonderful day!")
